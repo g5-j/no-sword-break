6427164232c6e18d05664597a05598c1f597a61d
ROOD <abdovop0@gmail.com>
2026-04-10 21:26:43 +0200

    Change BlockBreakMixin to use PlayerEntity

--- src/main/java/com/nbbs/aboodkt/mixin/BlockBreakMixin.java
+++ src/main/java/com/nbbs/aboodkt/mixin/BlockBreakMixin.java
@@ -1,31 +1,27 @@
 package com.nbbs.aboodkt.mixin;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
+import net.minecraft.world.World;
+import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ClientPlayerInteractionManager.class)
+@Mixin(PlayerEntity.class)
 public class BlockBreakMixin {
 
-    @Inject(method = "attackBlock", at = @At("HEAD"), cancellable = true)
-    private void noSwordBlockBreak(BlockPos pos, Direction direction, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "canHarvest", at = @At("HEAD"), cancellable = true)
+    private void noSwordBreak(BlockState state, CallbackInfoReturnable<Boolean> cir) {
 
-        MinecraftClient client = MinecraftClient.getInstance();
-
-        if (client.player == null) return;
-
-        ItemStack item = client.player.getMainHandStack();
+        PlayerEntity player = (PlayerEntity)(Object)this;
+        ItemStack item = player.getMainHandStack();
 
         if (item.getItem() instanceof SwordItem) {
             cir.setReturnValue(false);
-            cir.cancel();
         }
     }
 }
