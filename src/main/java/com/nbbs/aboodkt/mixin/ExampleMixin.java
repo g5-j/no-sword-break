bf05af65e4f607e1a36a0a76dafd26aebaa749eb
ROOD <abdovop0@gmail.com>
2026-04-10 20:15:32 +0200

    Change mixin target to ClientPlayerInteractionManager

--- src/main/java/com/nbbs/aboodkt/mixin/ExampleMixin.java
+++ src/main/java/com/nbbs/aboodkt/mixin/ExampleMixin.java
@@ -1,26 +1,31 @@
 package com.nbbs.aboodkt.mixin;
 
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.client.MinecraftClient;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerEntity.class)
+@Mixin(ClientPlayerInteractionManager.class)
 public class ExampleMixin {
 
-    @Inject(method = "interact", at = @At("HEAD"), cancellable = true)
-    private void preventSwordBlockUse(Hand hand, CallbackInfoReturnable<ActionResult> cir) {
-        PlayerEntity player = (PlayerEntity)(Object)this;
-        ItemStack item = player.getStackInHand(hand);
+    @Inject(method = "attackBlock", at = @At("HEAD"), cancellable = true)
+    private void cancelSwordBreak(BlockPos pos, Direction direction, CallbackInfoReturnable<Boolean> cir) {
+        MinecraftClient client = MinecraftClient.getInstance();
+
+        if (client.player == null) return;
+
+        ItemStack item = client.player.getMainHandStack();
 
         if (item.getItem() instanceof SwordItem) {
-            cir.setReturnValue(ActionResult.FAIL);
+            cir.setReturnValue(false);
         }
     }
 }
