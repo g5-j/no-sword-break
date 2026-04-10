b8baa9eecd59a3dfbdc2f7e2a576467d880b8959
ROOD <abdovop0@gmail.com>
2026-04-10 20:10:03 +0200

    Modify attack behavior to block sword interactions
    Prevent interaction with blocks when using a sword.

--- src/main/java/com/nbbs/aboodkt/mixin/ExampleMixin.java
+++ src/main/java/com/nbbs/aboodkt/mixin/ExampleMixin.java
@@ -1,15 +1,34 @@
 package com.nbbs.aboodkt.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.ActionResult;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
+@Mixin(MinecraftClient.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+
+    @Inject(method = "doAttack", at = @At("HEAD"), cancellable = true)
+    private void onAttack(CallbackInfoReturnable<Boolean> cir) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        ClientPlayerEntity player = client.player;
+
+        if (player == null) return;
+
+        ItemStack item = player.getMainHandStack();
+
+        if (item.getItem() instanceof SwordItem) {
+            // يمنع التفاعل مع البلوكات بالسيف
+            if (client.crosshairTarget instanceof BlockHitResult) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
 }
