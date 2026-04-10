3d482c81856c792c12de53bd8a24276d5505aa30
ROOD <abdovop0@gmail.com>
2026-04-10 21:09:09 +0200

    Modify block break behavior for players with swords

--- src/main/java/com/nbbs/aboodkt/mixin/BlockBreakMixin.java
+++ src/main/java/com/nbbs/aboodkt/mixin/BlockBreakMixin.java
@@ -4,24 +4,24 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public class BlockBreakMixin {
 
-    @Inject(method = "attack", at = @At("HEAD"), cancellable = true)
-    private void noSwordBlockBreak(CallbackInfo ci) {
-        PlayerEntity player = (PlayerEntity)(Object)this;
+    @Inject(method = "canHarvest", at = @At("HEAD"), cancellable = true)
+    private void noSwordBlockBreak(World world, net.minecraft.block.BlockState state, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
 
+        PlayerEntity player = (PlayerEntity)(Object)this;
         ItemStack heldItem = player.getMainHandStack();
 
-        // لو ماسك سيف، امنع الكسر/الضربة
+        // إذا ماسك سيف -> يمنع كسر البلوك
         if (heldItem.getItem() instanceof SwordItem) {
-            ci.cancel();
+            cir.setReturnValue(false);
         }
     }
 }
