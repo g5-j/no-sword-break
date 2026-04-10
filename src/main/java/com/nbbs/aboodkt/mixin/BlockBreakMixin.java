76734fe909da36e3b1cc7997f123d454414b27ac
ROOD <abdovop0@gmail.com>
2026-04-10 21:20:34 +0200

    Update BlockBreakMixin.java

--- src/main/java/com/nbbs/aboodkt/mixin/BlockBreakMixin.java
+++ src/main/java/com/nbbs/aboodkt/mixin/BlockBreakMixin.java
@@ -1,27 +1,32 @@
 package com.nbbs.aboodkt.mixin;
 
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.block.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerEntity.class)
+@Mixin(ClientPlayerInteractionManager.class)
 public class BlockBreakMixin {
 
-    @Inject(method = "canHarvest", at = @At("HEAD"), cancellable = true)
-    private void noSwordBlockBreak(World world, net.minecraft.block.BlockState state, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "attackBlock", at = @At("HEAD"), cancellable = true)
+    private void noSwordBlockBreak(BlockPos pos, Direction direction, CallbackInfoReturnable<Boolean> cir) {
 
-        PlayerEntity player = (PlayerEntity)(Object)this;
-        ItemStack heldItem = player.getMainHandStack();
+        MinecraftClient client = MinecraftClient.getInstance();
 
-        // إذا ماسك سيف -> يمنع كسر البلوك
-        if (heldItem.getItem() instanceof SwordItem) {
+        if (client.player == null) return;
+
+        ItemStack item = client.player.getMainHandStack();
+
+        if (item.getItem() instanceof SwordItem) {
             cir.setReturnValue(false);
+            cir.cancel();
         }
     }
 }
